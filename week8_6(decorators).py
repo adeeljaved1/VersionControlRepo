bb70f1edbfb26a47124ae188a3f7a6748fb2c122
Adeel Javed <adeel.javed@sheridancollege.ca>
2025-03-31 17:23:39 -0400

    decorator feature

--- week8_6(decorators).py
+++ week8_6(decorators).py
@@ -1,5 +1,6 @@
 # this is week 8's example, demonstrating decorators
 def my_decoractor(func):
+    print("this is test decorator feature")
     def wrapper():
         print("something is happening before hello")
         func()
